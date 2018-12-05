22f786fd55eb681c768fa5bdae6e19023ba309b0
Jesse Slavens <jesseslavens@Jesses-iMac.local>
2018-12-04 22:20:14 -0500

    initial commit

---
+++ src/components/HeaderColumn.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="col">
+    <div class="cell">
+      <span>JAMB</span>
+    </div>
+    <div class="cell">
+      <span>1</span>
+    </div>
+    <div class="cell">
+      <span>2</span>
+    </div>
+    <div class="cell">
+      <span>3</span>
+    </div>
+    <div class="cell">
+      <span>4</span>
+    </div>
+    <div class="cell">
+      <span>5</span>
+    </div>
+    <div class="cell">
+      <span>6</span>
+    </div>
+    <div class="cell">
+      <span>Total</span>
+    </div>
+    <div class="cell">
+      <span>Min</span>
+    </div>
+    <div class="cell">
+      <span>Max</span>
+    </div>
+    <div class="cell">
+      <span>Total</span>
+    </div>
+    <div class="cell">
+      <span>S</span>
+    </div>
+    <div class="cell">
+      <span>F</span>
+    </div>
+    <div class="cell">
+      <span>P</span>
+    </div>
+    <div class="cell">
+      <span>Y</span>
+    </div>
+    <div class="cell">
+      <span>Total</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderColumn"
+};
+</script>
+
+<style lang="scss">
+.col {
+  display: flex;
+  flex-direction: column;
+
+  .cell {
+    border: 1px solid #000;
+    text-align: center;
+    padding: 5px;
+  }
+}
+</style>
---
+++ src/components/ScoreCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="scorecard">
+    <HeaderColumn/>
+  </div>
+</template>
+
+<script>
+import HeaderColumn from "@/components/HeaderColumn.vue";
+export default {
+  name: "ScoreCard",
+  components: {
+    HeaderColumn
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.scorecard {
+  display: flex;
+  flex-direction: row;
+}
+</style>
